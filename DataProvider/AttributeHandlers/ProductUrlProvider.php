ce3a8693c674fc8bacf6088ceef8bada1b7a4864
Max Leung <maxandlin@gmail.com>
2024-07-30 16:24:34 -0400

    add google feed url

--- DataProvider/AttributeHandlers/ProductUrlProvider.php
+++ DataProvider/AttributeHandlers/ProductUrlProvider.php
@@ -8,39 +8,40 @@ use Magento\Catalog\Model\Product;
 use Magento\Framework\Url;
 use RunAsRoot\GoogleShoppingFeed\ConfigProvider\UrlSuffixProvider;
 use RunAsRoot\GoogleShoppingFeed\DataProvider\ParentProductProvider;
+use Magento\Framework\App\Config\ScopeConfigInterface;
 
 class ProductUrlProvider implements AttributeHandlerInterface
 {
     private Url $url;
     private ParentProductProvider $productProvider;
     private UrlSuffixProvider $urlSuffixProvider;
+    protected $scopeConfig;
 
     public function __construct(
         Url $url,
         ParentProductProvider $productProvider,
-        UrlSuffixProvider $urlSuffixProvider
+        UrlSuffixProvider $urlSuffixProvider,
+        ScopeConfigInterface $scopeConfig
     ) {
         $this->url = $url;
         $this->productProvider = $productProvider;
         $this->urlSuffixProvider = $urlSuffixProvider;
+        $this->scopeConfig = $scopeConfig;
     }
 
     public function get(Product $product): ?string
     {
         $productForUrlRetrieval = $this->productProvider->get($product);
 
+        $prefix_url = $this->scopeConfig->getValue("run_as_root_product_feed/general/url_prefix");
+
         $url = sprintf(
-            '%s%s',
+            '%s%s%s',
+            $prefix_url,
             'en/product/',
             $productForUrlRetrieval->getData('url_key'),
-            
+
         );
-
-        $routeParamsShort = [
-            '_direct' => $url,
-            '_nosid' => true,
-        ];
-
-        return $this->url->getUrl('', $routeParamsShort);
+        return $url;
     }
 }
