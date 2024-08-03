1282ac1863f226b3c5701d506744c04ae3b0d2e0
Max Leung <maxandlin@gmail.com>
2024-08-03 16:10:17 -0400

    update remove description html tag

---
+++ DataProvider/AttributeHandlers/DescriptionProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RunAsRoot\GoogleShoppingFeed\DataProvider\AttributeHandlers;
+
+use Magento\Catalog\Model\Product;
+use RunAsRoot\GoogleShoppingFeed\DataProvider\ParentProductProvider;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Store\Model\StoreManagerInterface;
+
+class DescriptionProvider implements AttributeHandlerInterface
+{
+    private ParentProductProvider $productProvider;
+    protected $scopeConfig;
+    protected $storeManager;
+
+    public function __construct(
+        ParentProductProvider $productProvider,
+        ScopeConfigInterface $scopeConfig,
+        StoreManagerInterface $storeManagerInterface
+    ) {
+
+        $this->productProvider = $productProvider;
+        $this->scopeConfig = $scopeConfig;
+        $this->storeManager = $storeManagerInterface;
+    }
+
+    public function get(Product $product): ?string
+    {
+        $description = strip_tags($product->getDescription());
+
+        return $description;
+    }
+}
